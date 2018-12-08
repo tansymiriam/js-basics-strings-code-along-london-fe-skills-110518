b836e73f31ebaa7e792444b66013f55fb46513bc
Tansy Roberts <tansymiriam@gmail.com>
2018-12-08 16:53:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
+<<<<<<< HEAD
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+=======
+const welcomeMessage = "Welcome to Flatbook, "
+>>>>>>> 78bef78ff2183a610686a5d8eacb5a27b3937445
